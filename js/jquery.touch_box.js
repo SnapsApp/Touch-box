7c6ede911d2fc18f7ec23c2bcc2d03819bfa328f
danniehansen <danniehansenweb@gmail.com>
2014-02-15 17:22:16 +0100

    + Added mouse support
    + Added mouse support for dragging
    + Updated example with new jQuery version
    + Fixed ghost images being displayed when dragging img tags

--- js/jquery.touch_box.js
+++ js/jquery.touch_box.js
@@ -1,5 +1,5 @@
 /**
-    Touch box - Enabled resize, drag & rotate of DOM elements on the web for iPad and other touch devices.
+    Touch box - Enabled resize, drag & rotate of DOM elements on the web touch devices & modern browsers.
     Copyright (C) 2013 Dannie Hansen
  
     This program is free software: you can redistribute it and/or modify
@@ -14,7 +14,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- **/
+**/
 ;(function ($) {
     "use strict";
     
@@ -75,123 +75,128 @@
             
             if(defaults.rotate) this.style[propNameTransformOrigin] = 'center center';
             
-            $this.bind('touchstart', function (e) {
-                zindex += 1;
-                
-                $this.css({ zIndex: zindex });
-                
-                touches = e.originalEvent.touches.length;
-                
-                if(ignoreTouch) ignoreTouch = false;
-                
-                if(!ignoreTouch) {
-                    var offsetLeft = parseFloat($this.css('left'),10),
-                        offsetTop = parseFloat($this.css('top'),10),
-                        x = e.originalEvent.touches[0].pageX,
-                        y = e.originalEvent.touches[0].pageY;
-                    
-                    startX = offsetLeft;
-                    startY = offsetTop;
-                    
-                    diffX = x - offsetLeft;
-                    diffY = y - offsetTop;
-                }
-                
-                if(defaults.rotate) {
-                    if(touches == 1) {
-                        rotatePoint1X = e.originalEvent.touches[0].pageX;
-                        rotatePoint1Y = e.originalEvent.touches[0].pageY;
-                    } else if(touches == 2) {
-                        rotatePoint2X = e.originalEvent.touches[1].pageX;
-                        rotatePoint2Y = e.originalEvent.touches[1].pageY;
-                        
-                        startDegree = getRotationAngle(rotatePoint1X, rotatePoint1Y, rotatePoint2X, rotatePoint2Y);
-                        _startDegree = currDegree;
-                        
-                        rotation = true;
-                    }
-                }
-                
-                if(defaults.resize && touches == 2) {
-                    startWidth = $this.width();
-                    startHeight = $this.height();
-                    var x = e.originalEvent.touches[0].pageX,
-                        y = e.originalEvent.touches[0].pageY,
-                        x2 = e.originalEvent.touches[1].pageX,
-                        y2 = e.originalEvent.touches[1].pageY,
-                        xd = x2 - x,
-                        yd = y2 - y,
-                        distance = Math.sqrt(xd*xd + yd*yd);
-                    
-                    startDistance = distance;
-                }
-            }).bind('touchmove', function (e) {
-                if(defaults.callback_touches != null) defaults.callback_touches.apply(this, [touches]);
-                
-                if(defaults.resize && touches == 2) {
-                    var x = e.originalEvent.touches[0].pageX,
-                        y = e.originalEvent.touches[0].pageY,
-                        x2 = e.originalEvent.touches[1].pageX,
-                        y2 = e.originalEvent.touches[1].pageY,
-                        xd = x2 - x,
-                        yd = y2 - y,
-                        distance = Math.sqrt(xd*xd + yd*yd),
-                        halfDistance = ((distance - startDistance)/2),
-                        newWidth = (startWidth + (distance - startDistance)),
-                        newHeight = (startHeight + (distance - startDistance)),
-                        newLeft = (startX-halfDistance),
-                        newTop = (startY-halfDistance);
-                    
-                    $this.css({
-                        width: newWidth+'px',
-                        height: newHeight+'px',
-                        left: newLeft+'px',
-                        top: newTop+'px'
-                    });
-                    
-                    if(defaults.callback_size_change != null) defaults.callback_size_change.apply(this, [newWidth, newHeight]);
-                    if(defaults.callback_position_change != null) defaults.callback_position_change.apply(this, [newLeft, newTop]);
-                }
-                
-                if(defaults.drag && !ignoreTouch && touches == 1) {
-                    var x = e.originalEvent.touches[0].pageX,
-                        y = e.originalEvent.touches[0].pageY,
-                        newLeft = (x-diffX),
-                        newTop = (y-diffY);
-                    
-                    $this.css({
-                        left: newLeft+'px',
-                        top: newTop+'px'
-                    });
-                    
-                    if(defaults.callback_position_change != null) defaults.callback_position_change.apply(this, [newLeft, newTop]);
-                }
-                
-                if(defaults.rotate && rotation) {
-                    var lastDegrees = currDegree,
-                    degrees = (startDegree - getRotationAngle(e.originalEvent.touches[0].pageX, e.originalEvent.touches[0].pageY, e.originalEvent.touches[1].pageX, e.originalEvent.touches[1].pageY) - _startDegree) * -1;
-                    currDegree = degrees;
-                    this.style[propNameTransform] = 'rotate('+Math.floor(degrees)+'deg)';
-                    
-                    if(defaults.callback_degree_change != null) defaults.callback_degree_change.apply(this, [lastDegrees, degrees]);
-                }
-                
-                e.preventDefault();
-            }).bind('touchend', function (e) {
-                touches -= 1;
-                
-                if(touches == 1) ignoreTouch = true;
-                
-                rotation = false;
-                
-                if(defaults.callback_touches != null) defaults.callback_touches.apply(this, [touches]);
+			$this[0]['draggable'] = false;
+			
+            $this.bind('touchstart mousedown', function (e) {
+				zindex += 1;
+				
+				$this.css({ zIndex: zindex });
+				
+				touches = (typeof e.pageX != 'undefined' ? 1 : e.originalEvent.touches.length);
+				
+				if(ignoreTouch) ignoreTouch = false;
+				
+				var pageX = (typeof e.pageX != 'undefined' ? e.pageX : e.originalEvent.touches[0].pageX),
+					pageY = (typeof e.pageY != 'undefined' ? e.pageY : e.originalEvent.touches[0].pageY);
+				
+				if(!ignoreTouch) {
+					var offsetLeft = parseFloat($this.css('left'),10),
+						offsetTop = parseFloat($this.css('top'),10),
+						x = pageX,
+						y = pageY;
+					
+					startX = offsetLeft;
+					startY = offsetTop;
+					
+					diffX = x - offsetLeft;
+					diffY = y - offsetTop;
+				}
+				
+				if(defaults.rotate) {
+					if(touches == 1) {
+						rotatePoint1X = pageX;
+						rotatePoint1Y = pageY;
+					} else if(touches == 2) {
+						rotatePoint2X = e.originalEvent.touches[1].pageX;
+						rotatePoint2Y = e.originalEvent.touches[1].pageY;
+						
+						startDegree = getRotationAngle(rotatePoint1X, rotatePoint1Y, rotatePoint2X, rotatePoint2Y);
+						_startDegree = currDegree;
+						
+						rotation = true;
+					}
+				}
+				
+				if(defaults.resize && touches == 2) {
+					startWidth = $this.width();
+					startHeight = $this.height();
+					var x = e.originalEvent.touches[0].pageX,
+						y = e.originalEvent.touches[0].pageY,
+						x2 = e.originalEvent.touches[1].pageX,
+						y2 = e.originalEvent.touches[1].pageY,
+						xd = x2 - x,
+						yd = y2 - y,
+						distance = Math.sqrt(xd*xd + yd*yd);
+					
+					startDistance = distance;
+				}
+            }).bind('touchmove mousemove', function (e) {
+				if(defaults.callback_touches != null) defaults.callback_touches.apply(this, [touches]);
+				
+				if(defaults.resize && touches == 2) {
+					var x = e.originalEvent.touches[0].pageX,
+						y = e.originalEvent.touches[0].pageY,
+						x2 = e.originalEvent.touches[1].pageX,
+						y2 = e.originalEvent.touches[1].pageY,
+						xd = x2 - x,
+						yd = y2 - y,
+						distance = Math.sqrt(xd*xd + yd*yd),
+						halfDistance = ((distance - startDistance)/2),
+						newWidth = (startWidth + (distance - startDistance)),
+						newHeight = (startHeight + (distance - startDistance)),
+						newLeft = (startX-halfDistance),
+						newTop = (startY-halfDistance);
+					
+					$this.css({
+						width: newWidth+'px',
+						height: newHeight+'px',
+						left: newLeft+'px',
+						top: newTop+'px'
+					});
+					
+					if(defaults.callback_size_change != null) defaults.callback_size_change.apply(this, [newWidth, newHeight]);
+					if(defaults.callback_position_change != null) defaults.callback_position_change.apply(this, [newLeft, newTop]);
+				}
+				
+				if(defaults.drag && !ignoreTouch && touches == 1) {
+					var x = (typeof e.pageX != 'undefined' ? e.pageX : e.originalEvent.touches[0].pageX),
+						y = (typeof e.pageY != 'undefined' ? e.pageY : e.originalEvent.touches[0].pageY),
+						newLeft = (x - diffX),
+						newTop = (y - diffY);
+					
+					$this.css({
+						left: newLeft+'px',
+						top: newTop+'px'
+					});
+					
+					if(defaults.callback_position_change != null) defaults.callback_position_change.apply(this, [newLeft, newTop]);
+				}
+				
+				if(defaults.rotate && rotation) {
+					var lastDegrees = currDegree,
+					degrees = (startDegree - getRotationAngle(e.originalEvent.touches[0].pageX, e.originalEvent.touches[0].pageY, e.originalEvent.touches[1].pageX, e.originalEvent.touches[1].pageY) - _startDegree) * -1;
+					currDegree = degrees;
+					this.style[propNameTransform] = 'rotate('+Math.floor(degrees)+'deg)';
+					
+					if(defaults.callback_degree_change != null) defaults.callback_degree_change.apply(this, [lastDegrees, degrees]);
+				}
+				
+				e.preventDefault();
+            }).bind('touchend mouseup', function (e) {
+				touches -= 1;
+				
+				if(touches == 1) ignoreTouch = true;
+				
+				rotation = false;
+				
+				if(defaults.callback_touches != null) defaults.callback_touches.apply(this, [touches]);
             });
         });
     };
     
     $(document).ready(function () {
         var $boxes = $('.touch-box');
-        
+		
         if($boxes.length > 0) {
             $boxes.each(function () {
                 var $this = $(this),
